e4a4365c9cb55076f35241b57e7bf57f3f2d31d1
salavina <saeid.alavi@mail.utoronto.ca>
2024-01-16 23:09:09 -0500

    requirements added

---
+++ setup.py
@@ -0,0 +1,31 @@
+# this file looks for every folder with __init__.py and installs the folder as a local package
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "End-to-end-ML-Project-with-MLflow"
+AUTHOR_USER_NAME = "salavina"
+SRC_REPO = "mlProject"
+AUTHOR_EMAIL = "saeid.alavi@mail.utoronto.ca"
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for ml app",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
